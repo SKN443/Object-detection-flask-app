0dda75b72c71b91abf082897d5bacaafd6a36d2b
SKN443 <hainguyen820428@gmail.com>
2024-05-27 00:51:07 +0700

    update code for virtual env

--- model.py
+++ model.py
@@ -3,15 +3,15 @@ from yolov5 import utils
 import os
 import shutil
 
-def object_detection(url):
+def object_detection(url): 
     file_name = os.path.basename(url)
     command = f'''python3 yolov5/detect.py --weights yolov5s.pt --img 640 \
-              --conf 0.25 --source {url} --name {file_name} 
-              --line-thickness 15'''
+              --conf 0.25 --source {url} --name {file_name} \
+              --hide-conf --line-thickness 3'''
     os.system(command)
     source_file = f"yolov5/runs/detect/{file_name}/{file_name}"
     destination_folder = "static/results/"
-    os.system("rm -r " + destination_folder+file_name)
+    os.system("rmdir /s /q " + destination_folder)
     shutil.copy(source_file, destination_folder)
     shutil.rmtree(f'yolov5/runs/detect/{file_name}')
     return f"results/result_{file_name}" 
